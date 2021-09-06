78209b7a677191d514451bae0a512cff8e87c06e
matiasilva <mw781@srcf.net>
2021-09-06 18:53:01 +0100

    Move around static files

---
+++ static/scripts/main.js
@@ -0,0 +1,13 @@
+// simple implementation of countdown timer
+
+const targetDate = new Date(2021, 10, 4); // month is 0-indexed
+
+function displayTimer(hello, hello2) {
+    
+};
+
+const intervalTimer = setInterval(() => {
+    const today = new Date();
+
+}, 1000);
+
