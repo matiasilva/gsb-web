97bd231e217eed732bc6f67e426aea1aac88e9a4
matiasilva <mw781@srcf.net>
2021-09-06 19:48:53 +0100

    Complete timer

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,13 +1,31 @@
 // simple implementation of countdown timer
 
 const targetDate = new Date(2021, 10, 4); // month is 0-indexed
+const timerElts = ["timerDays", "timerHours", "timerMinutes", "timerSeconds"].map((val) => document.getElementById(val));
 
-function displayTimer(hello, hello2) {
-    
+const ONESECOND = 1000;
+const ONEMINUTE = ONESECOND * 60;
+const ONEHOUR = ONEMINUTE * 60;
+const ONEDAY = ONEHOUR * 24;
+
+
+const displayTimer = diff => {
+    let cascadingDiff = diff;
+    let result = Array.from([ONEDAY, ONEHOUR, ONEMINUTE, ONESECOND], val => {
+        let tempVal = Math.floor(cascadingDiff / val)
+        cascadingDiff -= tempVal * val;
+        return tempVal;
+    });
+
+    timerElts.forEach((elt, index) => { elt.innerText = result[index] });
 };
 
-const intervalTimer = setInterval(() => {
+const calculateTimer = () => {
     const today = new Date();
+    const diff = targetDate - today;
+    displayTimer(diff);
+};
 
-}, 1000);
+calculateTimer();
+const intervalTimer = setInterval(calculateTimer, 1000);
 
